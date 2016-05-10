26876a8d05e4bcdbed2ad907a6438214475977b1
Michal Vyskocil <michalvyskocil@eaton.com>
2016-05-10 16:03:34 +0200

    Problem: no server implementation for cm
    Solution: add basic skeleton for cm_server processing and publishing
    messages
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/bios_cm_server.c
+++ src/bios_cm_server.c
@@ -38,31 +38,67 @@ struct _bios_cm_server_t {
 //  --------------------------------------------------------------------------
 //  Create a new bios_cm_server
 
-bios_cm_server_t *
-bios_cm_server_new (void)
-{
-    bios_cm_server_t *self = (bios_cm_server_t *) zmalloc (sizeof (bios_cm_server_t));
-    assert (self);
-    //  Initialize class properties here
-    return self;
-}
-
-
-//  --------------------------------------------------------------------------
-//  Destroy the bios_cm_server
-
 void
-bios_cm_server_destroy (bios_cm_server_t **self_p)
+bios_cm_server (zsock_t *pipe, void *args)
 {
-    assert (self_p);
-    if (*self_p) {
-        bios_cm_server_t *self = *self_p;
-        //  Free class properties here
-        //  Free object itself
-        free (self);
-        *self_p = NULL;
+    cmstats_t *stats = cmstats_new ();
+
+    // TODO: move to config file
+    static const size_t SIZE_LEN = 7;
+    static const char* STEPS[] = {"15m", "30m", "1h", "8h", "24h", "7d", "30d"};
+    static const uint64_t NSTEPS[] = {15*60, 30*60, 60*60, 8*3600, 24*3600, 7*24*3600, 30*24*3600};
+    static const size_t TYPES_LEN = 3;
+    static const char* TYPES[] = {"min", "max", "avg"};
+
+    mlm_client_t *client = mlm_client_new ();
+    //TODO: move to actor command
+    mlm_client_set_consumer (client, BIOS_PROTO_STREAM_METRICS, ".*");
+    mlm_client_set_producer (client, BIOS_PROTO_STREAM_METRICS);
+
+    zpoller_t *poller = zpoller_new (pipe, mlm_client_msgpipe (client), NULL);
+
+    while (!zsys_interrupted)
+    {
+        void *which = zpoller_wait (poller, 0);
+
+        if (!which || which == pipe)
+            break;
+
+        zmsg_t *msg = mlm_client_recv (client);
+        bios_proto_t *bmsg = bios_proto_decode (&msg);
+
+        //TODO: need to know the list of types and or devices to compute
+        if (!streq (bios_proto_type (bmsg), "realpower.default"))
+            continue;
+
+        for (size_t i = 0; i != SIZE_LEN; i++)
+        {
+            for (size_t j = 0; j != TYPES_LEN; j++)
+            {
+                bios_proto_t *stat_msg = cmstats_put (stats, TYPES [j], NSTEPS [j], bmsg);
+                if (stat_msg) {
+                    char *subject;
+                    asprintf (&subject, "%s_%s_%s@%s",
+                            bios_proto_type (stat_msg),
+                            TYPES [j],
+                            STEPS [i],
+                            bios_proto_element_src (stat_msg));
+
+                    zmsg_t *msg = bios_proto_encode (&stat_msg);
+                    mlm_client_send (client, subject, &msg);
+                }
+            }
+        }
+        bios_proto_destroy (&bmsg);
+
     }
+
+    zpoller_destroy (&poller);
+    mlm_client_destroy (&client);
+    cmstats_destroy (&stats);
 }
+
+
 
 //  --------------------------------------------------------------------------
 //  Self test of this class
@@ -73,10 +109,7 @@ bios_cm_server_test (bool verbose)
     printf (" * bios_cm_server: ");
 
     //  @selftest
-    //  Simple create/destroy test
-    bios_cm_server_t *self = bios_cm_server_new ();
-    assert (self);
-    bios_cm_server_destroy (&self);
+    //  TODO: testing
     //  @end
     printf ("OK\n");
 }
