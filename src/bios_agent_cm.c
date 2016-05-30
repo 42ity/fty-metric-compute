d084b741c8a2f8429bb807c68edbe1b5539dc654
Michal Vyskocil <michalvyskocil@eaton.com>
2016-05-30 08:33:39 +0200

    Problem: 24h computed metrics are not in the system
    Solution: use 24h and not 1d as a suffix for those averages
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/bios_agent_cm.c
+++ src/bios_agent_cm.c
@@ -79,7 +79,7 @@ int main (int argc, char *argv [])
     if (verbose)
         zstr_sendx (cm_server, "VERBOSE", NULL);
     zstr_sendx (cm_server, "TYPES", "min", "max", "arithmetic_mean", NULL);
-    zstr_sendx (cm_server, "STEPS", "15m", "30m", "1h", "8h", "1d", "7d", "30d", NULL);
+    zstr_sendx (cm_server, "STEPS", "15m", "30m", "1h", "8h", "24h", "7d", "30d", NULL);
     zstr_sendx (cm_server, "DIR", "/var/lib/bios/bios-agent-cm/", NULL);
     zstr_sendx (cm_server, "CONNECT", endpoint, "bios-cm-server", NULL);
     zstr_sendx (cm_server, "PRODUCER", BIOS_PROTO_STREAM_METRICS, NULL);
