4e1d1e0ab5ca79b10dde463316cd90da33609700
Chernikava, Alena <AlenaChernikava@Eaton.com>
2016-06-08 16:59:54 +0200

    Problem: tests are missing Solution: add test with empty structure+ comment
    Signed-off-by: Chernikava, Alena <AlenaChernikava@Eaton.com>

--- src/cmstats.c
+++ src/cmstats.c
@@ -33,7 +33,7 @@
 typedef void (compute_fn) (const bios_proto_t *bmsg, bios_proto_t *stat_msg);
 
 struct _cmstats_t {
-    zhashx_t *stats;
+    zhashx_t *stats; // a hash of BIOS_PROTO metrics for "AVG/MIN/MAX" ready to be published
 };
 
 static void
@@ -424,14 +424,24 @@ cmstats_test (bool verbose)
         printf ("\n");
 
     //  @selftest
-    //  Simple create/destroy test
 
     static const char *file = "src/cmstats.zpl";
     unlink (file);
 
+    if ( verbose )
+        zsys_info ("Test 1: Simple test on empty structure");
     cmstats_t *self = cmstats_new ();
     assert (self);
+    cmstats_print (self);
+    cmstats_delete_asset (self, "SOMESTRING");
+    // TODO uncomment, when tests for this function would be supported
+    // cmstats_poll (self, client, verbose);
+    cmstats_save (self, "itshouldbeemptyfile");
+    cmstats_destroy (&self);
 
+    if ( verbose )
+        zsys_info ("Test2: some test");
+    self = cmstats_new ();
     //XXX: the test is sensitive on timing!!!
     //     so it must start at the beggining of the second
     //     other option is to not test in second precision,
@@ -520,10 +530,11 @@ cmstats_test (bool verbose)
     //  1.6 check the arithmetic_mean
     stats = cmstats_put (self, "arithmetic_mean", "1s", 1, bmsg);
     assert (stats);
-    if (verbose)
+    if (verbose) {
         bios_proto_print (stats);
-    zsys_info ("avg real: %s", bios_proto_value (stats) );
-    zsys_info ("avg expected: %f", (100.99+42.1) / 2 );
+        zsys_info ("avg real: %s", bios_proto_value (stats) );
+        zsys_info ("avg expected: %f", (100.99+42.1) / 2 );
+    }
     assert (atof (bios_proto_value (stats)) == (100.99+42.1) / 2);
     assert (streq (bios_proto_aux_string (stats, AGENT_CM_COUNT, NULL), "2"));
     bios_proto_destroy (&bmsg);
