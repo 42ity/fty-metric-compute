88258ad0a0557e4eab390cd9622da64b3b9a0077
Jana Rapava <janarapava@eaton.com>
2017-01-10 13:14:46 +0100

    Problem: Debian packaging does not work.
    Solution: Regen; remove version definition regarding czmq so we would be able to build malamute.
    
    Signed-off-by: Jana Rapava <janarapava@eaton.com>

--- src/fty_metric_compute_selftest.c
+++ src/fty_metric_compute_selftest.c
@@ -36,7 +36,11 @@ typedef struct {
 
 static test_item_t
 all_tests [] = {
+// Tests for stable public classes:
     { "fty_mc_server", fty_mc_server_test },
+#ifdef FTY_METRIC_COMPUTE_BUILD_DRAFT_API
+    { "private_classes", fty_metric_compute_private_selftest },
+#endif // FTY_METRIC_COMPUTE_BUILD_DRAFT_API
     {0, 0}          //  Sentinel
 };
 
@@ -101,9 +105,8 @@ main (int argc, char **argv)
         if (streq (argv [argn], "--list")
         ||  streq (argv [argn], "-l")) {
             puts ("Available tests:");
-            puts ("    cmstats");
-            puts ("    cmsteps");
-            puts ("    fty_mc_server");
+            puts ("    fty_mc_server\t\t- stable");
+            puts ("    private_classes\t- draft");
             return 0;
         }
         else
